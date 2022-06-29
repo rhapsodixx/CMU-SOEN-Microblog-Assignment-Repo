2fdbe4ff31c0303ae7b0d1989f9bb94794fc784b
panji gautama <panji.gautama@gmail.com>
2022-06-30 06:40:03 +0700

    Fix wrong input getter

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -2,13 +2,15 @@ from flask import render_template, redirect, url_for, flash, request
 from werkzeug.urls import url_parse
 from flask_login import login_user, logout_user, current_user
 from flask_babel import _
-from app import db
+from app import create_app, db
 from app.auth import bp
 from app.auth.forms import LoginForm, RegistrationForm, \
     ResetPasswordRequestForm, ResetPasswordForm
 from app.models import User
 from app.auth.email import send_password_reset_email
 
+app = create_app()
+app.app_context().push()
 
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
@@ -16,8 +18,8 @@ def login():
         return redirect(url_for('main.index'))
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.name).first()
-        if user is None or not user.check_password(form.username.data):
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
             flash(_('Invalid username or password'))
             return redirect(url_for('auth.login'))
         login_user(user, remember=form.remember_me.data)
