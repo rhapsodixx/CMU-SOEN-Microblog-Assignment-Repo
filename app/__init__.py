0ada21e414b49e1aecca443bfae21fdfca2dca52
panji gautama <panji.gautama@gmail.com>
2022-09-06 17:15:57 +0700

    Init py should be excluded from flake8

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,6 @@ from elasticsearch import Elasticsearch
 from redis import Redis
 import rq
 from config import Config
-from app import models
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -95,3 +94,6 @@ def create_app(config_class=Config):
 @babel.localeselector
 def get_locale():
     return request.accept_languages.best_match(current_app.config['LANGUAGES'])
+
+
+from app import models
